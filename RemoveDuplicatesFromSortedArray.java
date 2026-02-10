7908c65154341c4d6a8b841bfb2e39a74c1390cd
Anjalirajak205 <anjalirajak205@gmail.com>
2026-02-11 00:04:43 +0530

    11 feb

---
+++ RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,31 @@
+public class RemoveDuplicatesFromSortedArray {
+
+    public static int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        int i = 1; // index for unique elements
+
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i - 1]) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+
+        return i; // number of unique elements
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 2, 2, 3,4,4};
+
+        int k = removeDuplicates(nums);
+        System.out.println("Unique count: " + k);
+
+        System.out.print("Array after removing duplicates: ");
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
