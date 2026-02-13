09faafe3202eead565b14d125412f018fee8beb0
Anjalirajak205 <anjalirajak205@gmail.com>
2026-02-13 14:28:53 +0530

    13 feb

---
+++ SearchInsertPosition.java
@@ -0,0 +1,31 @@
+public class SearchInsertPosition {
+
+    static int position(int[] nums, int target) {
+
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+
+        return low; 
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 3, 5, 6};
+
+        System.out.println(position(nums, 5)); 
+        System.out.println(position(nums, 2)); 
+        // System.out.println(position(nums, 7)); 
+        // System.out.println(position(nums, 0)); 
+    }
+}
